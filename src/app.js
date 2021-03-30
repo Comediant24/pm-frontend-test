bff4a2d0262f611a6996ca1d1f3dc6a5e2dbb9bc
Iurii Bairamukov <juribai@mail.ru>
2021-03-30 15:11:48 +0700

    feat: написать логику отображения поля с хобби/фото
    В app.js написать логику взаимодействия с узлами DOM
    скрывать узлы добавлением селектора hidden

--- src/app.js
+++ src/app.js
@@ -1,1 +1,35 @@
 import './scss/style.scss';
+
+const title = document.querySelector('.section-resume__title_about');
+
+const hobby = document.querySelector('.input_hobby');
+const photo = document.querySelector('.photo');
+
+const isHidden = (selector) => selector.classList.contains('hidden');
+const handleHidden = (selector) => selector.classList.toggle('hidden');
+
+const formToogle = () => {
+  let isHiddenPhoto = isHidden(photo);
+
+  if (!isHiddenPhoto) {
+    handleHidden(photo);
+    return;
+  }
+
+  setTimeout(() => {
+    if (!photo.classList.contains('hidden')) {
+      return;
+    }
+    handleHidden(hobby);
+  }, 200);
+};
+
+const photoToogle = () => {
+  handleHidden(photo);
+  if (!isHidden(hobby)) {
+    handleHidden(hobby);
+  }
+};
+
+title.addEventListener('dblclick', photoToogle);
+title.addEventListener('click', formToogle);
